6e0d571b0967354db12a1bb55b6d28de3457bd3f
Aveson <avesoncarvalho25@gmail.com>
2023-10-02 21:04:33 +0100

    place the colour at right row

--- scripts/code.js
+++ scripts/code.js
@@ -5,12 +5,18 @@ let turn = 1;
 function colourButton(button) {
 	selectedRow = checkActive(button);
 	console.log("lowest number is ", selectedRow);
-	// Colour depending on turn, and swap turn
+
+	// assigns button to the correct row
+	button = document.getElementById(`c${button.id.charAt(1)}r${selectedRow}`);
+
 	if (turn === 1) {
+		// Colour depending on turn, and swap turn
 		button.classList.add("red-buttons");
+		button.classList.add("active");
 		turn = 2;
 	} else {
 		button.classList.add("yellow-buttons");
+		button.classList.add("active");
 		turn = 1;
 	}
 }
@@ -18,12 +24,10 @@ function colourButton(button) {
 function checkActive(button) {
 	// variables for column number and row number
 	let columnNum = button.id.charAt(1);
-	let rowNum = button.id.substring(3);
 
 	// checks button under
 	let lowestRow = 6;
 	for (let index = 6; index >= 1; index--) {
-		console.log(index);
 		let indexButton = document.getElementById(`c${columnNum}r${index}`);
 		// Find lowest free row
 		lowestRow = index;
@@ -31,9 +35,6 @@ function checkActive(button) {
 		if ((index - 1 || index == 1) && indexButton.classList.contains("active")) {
 			// assigns lowest free row
 			lowestRow = index + 1;
-			if (lowestRow == 7) {
-				lowestRow = 6;
-			}
 			return lowestRow;
 		}
 	}
